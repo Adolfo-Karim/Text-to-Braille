606dd419a99817892910a71500a36cd168cc3ac5
Adolfo-Karim <adolfokarim@gmail.com>
2016-11-12 22:11:30 -0500

    All classes integrated into one, tests done, passed

--- serial_comunicator.py
+++ serial_comunicator.py
@@ -24,18 +24,40 @@ class Terminal (object):
 			self.writeMessage((user_in+'\n').encode())
 			print(self.ser.readline())
 
+
+	def binarize(self,stream):
+		binary_stream =[[],[]]
+		for row in [0,len(stream)//2]:
+			for col in range(cols):
+				binary = int(stream[row][col]+"00"+stream[row+1][col],2)
+				binary_stream[row//2][col]=binary
+		return binary_stream
+
+	def trivialize(self,stream):
+		new_stream = [[0]*len(stream[0]) for col in range(len(stream)//2)]
+		for row in [0,len(stream)//2]:
+			for col in range(len(stream[0])):
+				joined_strings = stream[row][col]+stream[row+1][col]
+				print(new_stream)
+				new_stream[row//2][col]= joined_strings
+		return new_stream
+
+
+
 	def input_stream(self,stream):
+		stream = self.trivialize(stream)
 		for row in range(len(stream)):
 			for col in range(len(stream[0])):
-				self.writeMessage((stream[row][col]+'\n').encode())
-				time.sleep(0.15)
-				print(self.ser.readline())
+				self.writeMessage((stream[row][col]+"\n").encode())
 
+				self.ser.flush()
+				print(self.ser.readline())
 		print("waiting for result")
 		time.sleep(5)
 
 
 term = Terminal("COM12")
+
 
 stream = [["111","011","101","111","000","010","001","010","101","101","001","001","101","110","101","011"],
 		  ["101","001","111","101","001","110","101","011","101","001","111","101","001","110","101","011"],
